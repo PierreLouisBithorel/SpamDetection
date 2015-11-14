df093ddd308135f1a40f9c6c06bb2a1e804db4b2
Pierre-Antoine ROBERT <pierreantoine.robert@gmail.com>
2015-11-14 19:17:07 +0100

    Added email class

--- base_mail.py
+++ base_mail.py
@@ -1,19 +1,12 @@
 from urllib2 import urlopen
-import tarfile
-import os
-import pandas
 from email import message_from_file
+import tarfile, os, pandas, re
 
 def downloadBases(currentDir):
-    """
-    Téléchargement des bases de données au format .tar.bz2 et extraction.
-    """
-    
+    """ Download and extract datas in format .tar.bz2. """
     depot = "http://spamassassin.apache.org/publiccorpus/"
     bases = ["20021010_easy_ham.tar.bz2", "20021010_hard_ham.tar.bz2", "20021010_spam.tar.bz2"]
-
     for i in bases:
-        baseName = i.split('_',1)[1].split('.',1)[0]
         remoteFile = urlopen(depot + i)
         localFile = open(currentDir + "/bases/" + i, 'wb')
         localFile.write(remoteFile.read())
@@ -23,8 +16,44 @@ def downloadBases(currentDir):
         tarDir.extractall(currentDir + "/bases/")
         tarDir.close()
         os.remove(currentDir + "/bases/" + i)
-
     return        
+
+class email:
+    """ Mail analysis and dataset construction. """
+
+    def __init__(self, adress):
+        emailMessage = message_from_file(open(adress, 'r'))
+        self.MessageID = emailMessage.get('Message-ID')
+        self.From = emailMessage.get('From')
+        self.Subject = emailMessage.get('Subject')
+        self.Date = emailMessage.get('Date')
+        self.Cc = self.removeSepInString(emailMessage.get('Cc'))
+        self.Encoding = emailMessage.get('Content-Transfer-Encoding')
+        self.Type = [emailMessage.get_content_maintype(), emailMessage.get_content_subtype()]
+        self.AttachedFile = emailMessage.get_filename()
+        self.IsMultipart = emailMessage.is_multipart()
+        self.Body = emailMessage.get_payload()
+
+    def removeSepInString(self, string):
+        """ Remove separators in an email list. """
+        print(string)
+        if string != None and string != '':
+            print('\n')
+            test = { }
+            print(string.split(','))
+            for email in string.split(','):
+                key = re.sub('[)"\']$', '', re.sub('^[\n\t\'(" ]*', '', re.sub('[ \n\t]*[<]?[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+[>]?', '', email)))
+                value = re.sub('>', '', re.sub('<', '', re.search('[<]?[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+[>]?', email).group(0)))
+                               # .split('<')[1].split('>')[0]
+                if key == '': key = None
+                print(key)
+                print(value)
+                test[value] = key
+        else:
+            test = None
+        print(test)
+        return test
+
 
 # Data storing from a distant repositorie.
 currentDir = os.getcwd()
@@ -37,19 +66,18 @@ if not os.path.exists(currentDir + "/bases/"):
 architecture = {type : os.listdir(currentDir + "/bases/" + type) for type in os.listdir(currentDir + "/bases")}
 error = [ ]
 
-df = pandas.DataFrame(columns = ['Message-ID', 'From', 'Subject', 'Date', 'Type', 'AttachedFile', 'Body'])
+df = pandas.DataFrame(columns = ['Message-ID', 'Type', 'From', 'Subject', 'Date', 'Forme', 'AttachedFile', 'MultiPart', 'Body'])
 row = 1
 
+i=1
 for type in architecture:
     for file in architecture[type]:
-        emailMessage = message_from_file(open(currentDir + "/bases/" + type + "/" + file, 'r'))
-        try:
-            df.loc[row] = [emailMessage.get('Message-ID'), emailMessage.get('From'), emailMessage.get('Subject'), emailMessage.get('Date'), emailMessage.get_content_type().split('/',1)[0], emailMessage.get_filename(), emailMessage.get_payload().split()]
-        except:
-            error.append(currentDir + "/bases/" + type + "/" + file)
-        row += 1
-
-print(len(error))
-
+        email(currentDir + "/bases/" + type + "/" + file)
+        i+=1
+        print(i)
+        
 # df.to_csv(currentDir + "/bases/base.txt", sep="\t", index=False)
-# print(emailMessage.keys()) # Return all items in the header
+
+
+
+# emailMessage = message_from_file(open("/home/pierre-antoine/Documents/Cours/Python/SpamDetection/bases/spam/0064.d700742b9815d990b2e5a7921e8d854c",'r'))
